474cf6d2c6cf9f91304b902267a9dca272285c28
Sachin rajput <sachin108rajput@hotmail.com>
2022-04-30 11:02:59 +0530

    30th April

---
+++ base/forms.py
@@ -0,0 +1,9 @@
+import imp
+from pyexpat import model
+from django.forms import ModelForm
+from .models import Room
+
+class RoomForm(ModelForm):
+    class Meta:
+        fields = '__all__'
+        model = Room 
--- base/views.py
+++ base/views.py
@@ -1,23 +1,56 @@
+from multiprocessing import context
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Room
+from .forms import RoomForm
 
+'''
 rooms=[
     {'id':1, 'name':'C++'},
     {'id':2, 'name':'Java'},
     {'id':3, 'name':'GoLang'},
     {'id':4, 'name':'Scala'},
 ]
-
+'''
 # Create your views here.
 def Home(request):
+    rooms=Room.objects.all()
     context={'rooms':rooms}
     return render(request, 'base/home.html', context)
 
-def Room(request, pk):
-    room=None
-    for i in rooms:
-        if i['id']==int(pk):
-            room=i
-        context={'room':room}
+def room(request, pk):
+    room=Room.objects.get(id=pk)
+    context={'room':room}
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form=RoomForm()
+    if(request.method=='POST'):
+        form=RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('Home')
+
+    context={'form':form}
+    return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room=Room.objects.get(id=pk)
+    form=RoomForm(instance=room)
+    
+    if request.method=='POST':
+        form=RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('Home')
+    
+    context={ 'form':form }
+    return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+    room=Room.objects.get(id=pk)
+    if request.method=='POST':
+        room.delete()
+        return redirect('Home')
+    
+    return render(request, 'base/delete.html', {'obj':room})

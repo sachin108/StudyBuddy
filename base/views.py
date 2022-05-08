61c21484024ea5ccb06c151e82d28d8f691ddc5a
Sachin rajput <sachin108rajput@hotmail.com>
2022-05-09 00:06:47 +0530

    9th-May

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 from email import message
+from email.policy import default
 from multiprocessing import context
 from django.contrib import messages
 from django.http import HttpResponse
@@ -65,11 +66,12 @@ def logoutUser(request):
     return redirect('Home')
     
 def Home(request):
-    q=request.GET.get('q') 
-    rooms=Room.objects.all( )
+    q=request.GET.get('q', default="") 
+    rooms=Room.objects.all()
     room_count=rooms.count()
     topics=Topic.objects.all()
-    context={'rooms':rooms, 'topics':topics, 'room_count':room_count}
+    room_messages=Message.objects.filter(Q(room__topic__name__icontains=q))
+    context={'rooms':rooms, 'topics':topics, 'room_count':room_count, "room_messages":room_messages}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
@@ -87,6 +89,14 @@ def room(request, pk):
 
     context={'room':room, 'room_messages':room_messages, "participants":participants}
     return render(request, 'base/room.html', context)
+
+def userProfile(request, pk):
+    user=User.objects.get(id=pk)
+    rooms=user.room_set.all()
+    room_messages=user.message_set.all()
+    topics=Topic.objects.all()
+    context={"user":user, "rooms":rooms, "room_messages":room_messages, "topics":topics}
+    return render(request, 'base/profile.html', context)
 
 @login_required(login_url='login')  #2h44m
 def createRoom(request):

27d3746845187e152b2d815c36c3a11456d87a59
Sachin rajput <sachin108rajput@hotmail.com>
2022-05-20 00:43:01 +0530

    20th may

--- StudyBuddy/urls.py
+++ StudyBuddy/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
+    path('api/', include('base.api.urls')),
 ]
 
 '''urlpatterns store the url of a view for a page'''
---
+++ base/api/views.py
@@ -0,0 +1,11 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes=[
+        'GET /api',
+        'GET /api/rooms',
+        'GET /api/rooms/:id'
+    ]
+    return Response(routes)
